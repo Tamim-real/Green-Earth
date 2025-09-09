f303c33b5388a8db85bdec85531d82b14d37c4db
Tamim-real <tamimislam2653@gmail.com>
2025-09-09 18:12:17 +0600

    about section added"

--- script.js
+++ script.js
@@ -9,27 +9,63 @@ const loadAllPlants = ()=>{
   .then(data=> displayAllPlants(data.plants))
 }
 
+const loadModalDetail= async (id)=>{
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`
+    console.log(url);
+    const res =  await fetch(url);
+
+    const detail =  await res.json();
+    displayModalDetail(detail.plants);
+    
+    
+}
+
+const displayModalDetail=(word)=>{
+    console.log(word);
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML = `
+    <div class="card bg-base-100 w-96 h-96 shadow-sm">
+  <figure class="px-10 pt-10">
+    <img
+      src="${word.image}"
+      alt="Shoes"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-start text-left">
+    <h2 class="card-title">${word.name}</h2>
+    <p><span class="font-bold">Category :</span> ${word.category}</p>
+    <p> <span class="font-bold">Price :</span> ${word.price}</p>
+
+    <p><span class="font-bold">Description :</span> ${word.description}</p>
+    
+  </div>
+</div>
+    `
+
+    document.getElementById('my_modal_2').showModal();
+}
+
 const displayAllPlants =(allPlants)=>{
       const cardContainer = document.getElementById('card-container');
     
       for(const plants of allPlants){
         const card = document.createElement('div');
         card.innerHTML = `
-         <div class="card bg-base-100 w-80 h-96 shadow-sm">
+         <div class="card bg-base-100 w-80 h-96 shadow-sm  ">
   <figure>
     <img
       src="${plants.image}"
       alt="Shoes" class="mt-5" />
   </figure>
   <div class="card-body">
-    <h2 class="card-title">
+    <h2 class="card-title" onclick="loadModalDetail(${plants.id})">
       ${plants.name}
       
     </h2>
     <p>${plants.description}</p>
     <div class="card-actions justify-between">
         <div class="badge  bg-[#CFF0DC] text-[#15803D]">${plants.category}</div>
-       <div> <p> ${plants.price}</p></div>
+       <div><p class="item-price-minus">৳ ${plants.price}</p></div>
       
     </div>
     <button class="bg-[#15803D] text-white rounded-3xl px-4 h-[38px] text-xs font-semibold ">Add to Cart</button>
@@ -82,12 +118,12 @@ const showCartItems=(cartItems)=>{
       const cartDiv = document.createElement('div');
       
 
-      cartDiv.innerHTML =`<div class="bg-[#CFF0DC] px-7 py-3 rounded-lg flex justify-between items-center gap-10 mb-5">
+      cartDiv.innerHTML =`<div class="bg-[#CFF0DC] px-7 py-3 rounded-lg flex justify-between cart-item items-center gap-10 mb-5">
                         <div>
                             <h2>${item.title}</h2>
-                        <p>৳<span>${item.price}</span></p>
+                        <p><span class="item-price" >${item.price}</span></p>
                         </div>
-                        <div><span>&times;</span></div>
+                        <div class="rmvBtn">&times;</div>
                     </div>
       
       `
@@ -95,6 +131,8 @@ const showCartItems=(cartItems)=>{
       cartContainer.append(cartDiv)
     })
 }
+
+
 
 loadAllPlants()
 
@@ -125,7 +163,7 @@ const displayCards =(details)=>{
       alt="Shoes" class="mt-5" />
   </figure>
   <div class="card-body">
-    <h2 class="card-title">
+    <h2 class="card-title" onclick="my_modal_2.showModal()">
       ${detail.name}
       
     </h2>
@@ -179,6 +217,57 @@ const displayCategories=(catagories)=>{
     })
         
 }
+
+let totalAmount = 0
+let finalAmount = document.getElementById('final-amount');
+
+document.getElementById('card-container').
+addEventListener('click', (e)=>{
+  let itemAmount = parseInt((e.target.previousElementSibling.children[1].innerText).replace(/[^\d.]/g, ""));
+  totalAmount+= itemAmount
+
+  
+
+  finalAmount.innerText = totalAmount
+
+
+  
+  
+})
+
+
+
+  
+  cartContainer.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('rmvBtn') ){
+
+      let minusPrice = parseInt(e.target.parentNode.children[0].lastElementChild.innerText.replace(/[^\d.]/g, ""));
+      
+
+      totalAmount -= minusPrice
+    finalAmount.innerText = totalAmount
+      
+
+      
+
+     
+      e.target.parentNode.remove()
+      
+     
+     
+    
+    
+
+      
+      
+      
+    }
+
+
+    
+})
+
+
 
 loadCategories()
 
